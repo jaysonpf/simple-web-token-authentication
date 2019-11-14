c29084d04d8c6e3a3c9ec0ba5038c8be96cc5929
Jayson Freitas <jaysonpf@gmail.com>
2019-11-13 22:43:06 -0300

    add health check endpoint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const restify = require("restify");
 const bunyan = require("bunyan");
-const mongoose = require("mongoose");
 const singIn = require("./modules/authentication/routes/sign-in");
 const singOut = require("./modules/authentication/routes/sign-out");
 const verify = require("./modules/authentication/routes/verify");
+const healthCheck = require("./modules/authentication/routes/health-check");
 
 let log = bunyan.createLogger({
 	name: "simple-web-token-authentication",
@@ -15,31 +15,23 @@ let log = bunyan.createLogger({
 
 log.info("starting server... ");
 
-mongoose.connect("mongodb://localhost:27017/token-test", {useNewUrlParser: true} )
-.then(()=>{
-	let serverOptions = {
-		name: "simple-web-token-authentication",
-		port: 8085,
-		log: log		
-	};
-	let server = restify.createServer(serverOptions);
-	server.use(restify.plugins.queryParser());
-	server.use(restify.plugins.bodyParser({
-		mapParams: false
-	}));
+let serverOptions = {
+	name: "simple-web-token-authentication",
+	port: 8085,
+	log: log
+};
+let server = restify.createServer(serverOptions);
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser({
+	mapParams: false
+}));
 
-	server.post("sign-in", singIn);
-	server.post("sign-out", singOut);
-	server.post("verify", verify);
+server.post("sign-in", singIn);
+server.post("sign-out", singOut);
+server.post("verify", verify);
+server.get("health-check", healthCheck);
 
 
-	server.listen(serverOptions.port, function () {
-		log.info("%s listening at %s", server.name, server.url);
-	});
-
-})
-.catch(err =>{
-
-	log.error("error establishing a database connection", err);
-		process.exit(1);
+server.listen(serverOptions.port, function () {
+	log.info("%s listening at %s", server.name, server.url);
 });
